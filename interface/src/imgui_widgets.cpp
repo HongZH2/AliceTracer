b2a5ea54e6e5c34b5c81b0c819607f1c1d3e3b7c
zhanghong50 <m2clarry@gmail.com>
2022-11-01 20:33:11 +0800

    Update imgui_widgets.cpp

--- interface/src/imgui_widgets.cpp
+++ interface/src/imgui_widgets.cpp
@@ -69,13 +69,7 @@ namespace ALICE_TRACER{
     void ImGUIWidget::drawWidgets() {
         {
             ImGui::DockSpaceOverViewport(ImGui::GetMainViewport(), ImGuiDockNodeFlags_PassthruCentralNode);
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+            ImGui::Begin("Hello, Alice Tracer!");
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::End();
         }
